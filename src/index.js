9488510af63dd4d6c7ae02f58430d0230cc3b839
dhgiang <dhgiang.vu@gmail.com>
2024-03-08 16:34:45 +1100

    implemented change tab mechanic

--- src/index.js
+++ src/index.js
@@ -1,2 +1,51 @@
 import './style.css'
-console.log("Hello World");
+
+
+const sidebarNav = document.querySelector(".nav-list");
+const sidebarNavLinks = document.querySelectorAll('.nav-list a');
+const mainContentPgs = document.querySelector(".main-content").children;
+
+
+
+
+const displayController = (function() {
+    let activeIndex = 0;
+    
+    const removeActive = () => {
+        sidebarNavLinks[activeIndex].classList.remove("active");
+        mainContentPgs[activeIndex].classList.remove("active");
+    }
+
+    const makeActive = () => {
+        sidebarNavLinks[activeIndex].classList.add("active");
+        mainContentPgs[activeIndex].classList.add("active");
+    }
+
+    const changeActiveTab = (newIndex) => {
+        removeActive();
+        activeIndex = newIndex;
+        makeActive();
+    }
+    
+    // Display page with index === 0 on page load 
+    window.onload = makeActive();
+
+    return {changeActiveTab}
+})();
+
+
+
+
+
+
+
+
+sidebarNav.addEventListener("click", function(e) {
+    if (e.target.tagName !== 'A') {
+        return;
+    }
+    const newActiveIndex = e.target.getAttribute('index');
+    displayController.changeActiveTab(newActiveIndex);
+})
+
+
